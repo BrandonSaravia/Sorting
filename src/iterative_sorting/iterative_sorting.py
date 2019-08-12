236bc5cd7df8f4a3cd0a51ec8bcaf3d0c0dab8ca
BrandonSaravia <BPSaravia@gmail.com>
2019-08-12 19:21:18 -0400

    Finished mvp

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -21,8 +21,12 @@ def selection_sort(arr):
 
 
 # TO-DO:  implement the Bubble Sort function below
-def bubble_sort( arr ):
-    
+def bubble_sort(arr):
+    for i in range(1, len(arr)):
+        j = i
+        while j > 0 and arr[j] < arr[j - 1]:
+            arr[j], arr[j - 1] = arr[j - 1] , arr[j]
+            j-=1
     return arr
 
 
